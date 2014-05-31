1e6ac1e4fe39f7c29eaf28ea241a9587170e440c
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-06-01 00:31:46 +0800

    2.新功能：安装dedecms

---
+++ db/zhongyuan-security.sql
@@ -0,0 +1,9 @@
+-- 2014-05-31
+create database zhongyuan_security character set utf8 collate utf8_general_ci;
+GRANT ALL ON `zhongyuan_security`.* TO `zhongyuan`@localhost IDENTIFIED BY 'zhongyuan123';
+FLUSH PRIVILEGES;
+
+drop database zhongyuan_security;
+
+select * from zhongyuan_sysconfig;
+select * from zhongyuan_sysconfig where value like '%dede%';
---
+++ src/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'zhongyuan_security';
+$cfg_dbuser = 'zhongyuan';
+$cfg_dbpwd = 'zhongyuan123';
+$cfg_dbprefix = 'zhongyuan_';
+$cfg_db_language = 'utf8';
+
+
+?>
