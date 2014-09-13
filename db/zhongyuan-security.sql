4ac345bf9993d8c14d5765a3064acb6ef15891a3
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-09-14 00:46:35 +0800

    1.新功能:增加财经专家列表页

--- db/zhongyuan-security.sql
+++ db/zhongyuan-security.sql
@@ -253,3 +253,44 @@ select * from zhongyuan_bbs.zhongyuan_common_stylevar;
 select * from zhongyuan_bbs.zhongyuan_common_stylevar where substitute like '%iscwo_design_2013%';
 
 select * from zhongyuan_bbs.zhongyuan_forum_forum;
+
+-- 2014-09-14
+select * from zhongyuan_security.zhongyuan_arctype;
+show create table zhongyuan_security.zhongyuan_arctype;
+CREATE TABLE `zhongyuan_arctype` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `reid` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `topid` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `sortrank` smallint(5) unsigned NOT NULL DEFAULT '50',
+  `typename` char(30) NOT NULL DEFAULT '',
+  `typedir` char(60) NOT NULL DEFAULT '',
+  `isdefault` smallint(6) NOT NULL DEFAULT '0',
+  `defaultname` char(15) NOT NULL DEFAULT 'index.html',
+  `issend` smallint(6) NOT NULL DEFAULT '0',
+  `channeltype` smallint(6) DEFAULT '1',
+  `maxpage` smallint(6) NOT NULL DEFAULT '-1',
+  `ispart` smallint(6) NOT NULL DEFAULT '0',
+  `corank` smallint(6) NOT NULL DEFAULT '0',
+  `tempindex` char(50) NOT NULL DEFAULT '',
+  `templist` char(50) NOT NULL DEFAULT '',
+  `temparticle` char(50) NOT NULL DEFAULT '',
+  `namerule` char(50) NOT NULL DEFAULT '',
+  `namerule2` char(50) NOT NULL DEFAULT '',
+  `modname` char(20) NOT NULL DEFAULT '',
+  `description` char(150) NOT NULL DEFAULT '',
+  `keywords` varchar(60) NOT NULL DEFAULT '',
+  `seotitle` varchar(80) NOT NULL DEFAULT '',
+  `moresite` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `sitepath` char(60) NOT NULL DEFAULT '',
+  `siteurl` char(50) NOT NULL DEFAULT '',
+  `ishidden` smallint(6) NOT NULL DEFAULT '0',
+  `cross` tinyint(1) NOT NULL DEFAULT '0',
+  `crossid` text,
+  `content` text,
+  `smalltypes` text,
+  PRIMARY KEY (`id`),
+  KEY `reid` (`reid`,`isdefault`,`channeltype`,`ispart`,`corank`,`topid`,`ishidden`),
+  KEY `sortrank` (`sortrank`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+alter table zhongyuan_security.zhongyuan_arctype change column `typedir` `typedir` char(255) NOT NULL DEFAULT '';
